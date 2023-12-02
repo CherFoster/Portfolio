2bce58f539166e071ab0cc32aa2cf6f3dc18868f
Cherise Foster <cherise.foster@outlook.com>
2023-12-02 11:03:01 -0700

    added links to socials

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../styles/Header.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Link } from 'react-router-dom';
+import Navbar from "./NavBar";
 
 function Header() {
   return (
@@ -12,23 +13,20 @@ function Header() {
         <h2>I'm a full-stack <span>software engineer</span> from Denver, Colorado</h2>
         <p className="my-3 text-white">I bring innovative solutions to complex problems, blending front-end flair with robust backend functionalities. My focus is on creating user-friendly and efficient applications, always prioritizing a seamless user experience. My journey in tech is driven by a love for learning and exploring new technologies, constantly enhancing both the user experience and my own skill set.</p>
 
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li><Link className="nav-link" to="/">Home</Link></li>
-            <li><Link className="nav-link" to="/about">About</Link></li>
-            <li><Link className="nav-link" to="/resume">Resume</Link></li>
-            <li><Link className="nav-link" to="/portfolio">Portfolio</Link></li>
-            <li><Link className="nav-link" to="/contact">Contact</Link></li>
-          </ul>
-          <i className="bi bi-list mobile-nav-toggle"></i>
-        </nav>
+        <Navbar />
 
         <p className="my-3">Check out my LinkedIn profile, GitHub, and blog:</p>
   
         <div className="social-links">
-          <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
-          <a href="#" className="github"><i class="bi bi-github"></i></a>
-          <a href="#" className="blog"><i className="bi bi-journal-text"></i></a>
+          <Link className="linkedin" to="https://www.linkedin.com/in/cherise-foster/">
+            <i className="bi bi-linkedin"></i>
+            </Link>
+          <Link className="github" to="https://github.com/CherFoster">
+            <i class="bi bi-github"></i>
+            </Link>
+          <Link className="blog" to="https://dev.to/cherisefoster">
+            <i className="bi bi-journal-text"></i>
+            </Link>
           
         </div>
 
