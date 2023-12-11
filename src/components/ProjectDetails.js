fbb6e04af34e51157d6227e2c53a1e1931e4692c
Cherise Foster <cherise.foster@outlook.com>
2023-12-11 13:14:00 -0700

    done

---
+++ src/components/ProjectDetails.js
@@ -0,0 +1,33 @@
+export const projectDetails =[
+    {
+        name: "Advanced Rescue Communication (ARC)",
+        image: "https://i.postimg.cc/MKDC5gVX/ARC.jpg",
+        summary: "A web application designed to assist in the communication of rescue teams during emergencies",
+        github: "https://github.com/CherFoster/ARC",
+        video: "https://youtu.be/Ary3HqyHras",
+        technologies: "JavaScript, React, Python, Flask, Redux, Bootstrap, Formik, Bcrypt, HTML, CSS",
+        description: "This innovative application utilizes a variety of modern web technologies to streamline evacuation processes during emergencies. The front end is developed with Bootstrap, ensuring a responsive and mobile-friendly user interface. Redux is employed for state management across the platform, offering a predictable state container. The application's backend is built using Python with Flask. Other technologies involved include Node.js for client-side management and Pipenv for managing Python dependencies. The project features include account creation and sign-in, profile management, a user directory, evacuation addresses listing, status updates, and detailed residence information, all facilitated by Redux for real-time updates and consistency.",
+        icon: <i class="bi bi-prescription2"></i>
+    },
+    {
+        name: "BookHub",
+        image: "https://i.postimg.cc/nz5LGyJh/Bookhub-1.png",
+        summary: "A user-centric platform for discovering, managing, and reviewing books",
+        github: "https://github.com/CherFoster/BookHub",
+        video: "https://youtu.be/hNn79uTcrGM",
+        technologies: "JavaScript, React, Python, Flask, Bootstrap, Formik, Bcrypt, HTML, CSS",
+        description: "This web application is designed for book lovers. It offers features such as user authentication, book management where users can upload book details and mark them as 'Read' or 'Want to Read,' and book reviews. Utilizing Bootstrap for a responsive front-end design, BookHub also includes Node.js for client-side management and Python for the server-side. This project is ideal for avid readers and book enthusiasts who want to track and share their reading experiences.",
+        icon: <i class="bi bi-book"></i>
+    },
+    {
+        name: "AirBook",
+        image: "https://i.postimg.cc/nz66X6Sf/airbook.png",
+        summary: "A command-line application for viewing and managing flight, passenger, and reservation details, including creating and deleting reservations",
+        github: "https://github.com/CherFoster/Phase-3-Project",
+        video: "https://youtu.be/9UlGwEZiEGQ",
+        technologies: "Python, SQL, SQLAlchemy, Alembic",
+        description: "AirBook is a database application that includes features for viewing, creating, and deleting reservations for flight information. The project uses Python and SQLAlchemy. The application structure comprises a main interactive file (cli.py) with various functions, and a database structure in Models.py. The Seed.py file is used for data seeding. The project includes helper functions for managing flight, passenger, and reservation data, allowing users to perform CRUD operations on reservations and view detailed flight and passenger information. ",
+        icon: <i class="bi bi-airplane"></i>
+    },
+]
+
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,69 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { projectDetails } from "./ProjectDetails";
 import '../styles/Projects.css'
 
 function Projects({ onHideOverlay }){
-    return (
-        <section id="portfolio" className="portfolio">
-            <button onClick={onHideOverlay} className="btn btn-secondary">
-          Back
-        </button>
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  function handleProjectClick(projectName) {
+    const project = projectDetails.find(p => p.name === projectName);
+    setSelectedProject(project);
+  };
+
+  return (
+    <section id="portfolio" className="portfolio">
+      {selectedProject ? (
+        // Render the selected project's details
+        <div className="portfolio-details">
+          <button onClick={() => setSelectedProject(null)} className="btn btn-secondary my-5">
+            Back
+            </button>
           <div className="container">
-    
-            <div className="section-title">
-              <h2 className="text-center text-white">Projects</h2>
+            <h3 className="portfolio-title">{selectedProject.icon} {selectedProject.name}</h3>
+            <div className="portfolio-info">
+              <img className="project-image" src={selectedProject.image} />
+              <p>{selectedProject.summary}</p>
+              <p>{selectedProject.description}</p>
+              <p>Technologies: {selectedProject.technologies}</p>
+              <p>Check out my Github repository and video walkthrough:</p>
+              <div className="project-links">
+              <a href={selectedProject.github} className="github">
+                    <i className="bi bi-github"></i>
+                  </a>
+                  <a href={selectedProject.video} className="github">
+                    <i class="bi bi-youtube"></i>
+                  </a>
+                
+                  </div>
             </div>
-    
-            <div className="row portfolio-container my-3">
-              <div className="col-lg-4 col-md-4 portfolio-item">
-                <div className="portfolio-wrap">
-                  <img src="https://i.postimg.cc/MKDC5gVX/ARC.jpg" className="img-fluid" alt="" />
-                  <div className="portfolio-info">
-                    <h4>Advanced Rescue Communications</h4>
-                    <p>A web application designed to assist in the communication of rescue teams during emergencies.</p>
-                  </div>
-                </div>
-                <p className="my-4">JavaScript, React, Python, Flask, Redux, Bootstrap, Formik, Bcrypt, HTML, CSS</p>
-              </div>
-
-              <div className="col-lg-4 col-md-4 portfolio-item">
-                <div className="portfolio-wrap">
-                  <img src="https://i.postimg.cc/nz5LGyJh/Bookhub-1.png" className="img-fluid" alt="" />
-                  <div className="portfolio-info">
-                    <h4>BookHub</h4>
-                    <p>A user-centric platform for discovering, managing, and reviewing books</p>
-                  </div>
-                </div>
-                <p className="my-4">JavaScript, React, Python, Flask, Bootstrap, Formik, Bcrypt, HTML, CSS</p>
-              </div>
-
-              <div className="col-lg-4 col-md-4 portfolio-item">
-                <div className="portfolio-wrap">
-                  <img src="https://i.postimg.cc/nz66X6Sf/airbook.png" className="img-fluid" alt="" />
-                  <div className="portfolio-info">
-                    <h4>AirBook</h4>
-                    <p>A command-line application for viewing and managing flight, passenger, and reservation details, including creating and deleting reservations</p>
-                  </div>
-                </div>
-                <p className="my-4">Python, SQL, SQLAlchemy, Alembic</p>
-              </div>
-
-              {/* <div className="col-lg-4 col-md-4 portfolio-item">
-                <div className="portfolio-wrap">
-                  <img src="https://i.postimg.cc/MKDC5gVX/ARC.jpg" className="img-fluid" alt="" />
-                  <div className="portfolio-info">
-                    <h4>Advanced Rescue Communications</h4>
-                    <p>A web application designed to assist in the communication of rescue teams during emergencies.</p>
-                  </div>
-                </div>
-                <p className="my-4">dfsdf</p>
-              </div> */}
-  
-            </div>
-    
           </div>
-        </section>
-      );
-    };
-
+        </div>
+      ) : (
+        // Render the list of all projects
+        <div className="container">
+          <button onClick={onHideOverlay} className="btn btn-secondary details-button">Back</button>
+          <div className="section-title">
+            <h2 className="text-center text-white">Projects</h2>
+          </div>
+          <div className="row portfolio-container my-3">
+            {projectDetails.map(project => (
+              <div key={project.name} className="col-lg-4 col-md-4 portfolio-item" onClick={() => handleProjectClick(project.name)}>
+                <div className="portfolio-wrap">
+                  <img src={project.image} className="img-fluid" alt={project.name} />
+                  <div className="portfolio-info">
+                    <h4>{project.name}</h4>
+                    <p>{project.summary}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </section>
+  );
+}
 
 export default Projects;
